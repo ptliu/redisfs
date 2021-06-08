054f81d60a6a6cee26d8f3688315c46a719c2e93
Thiago Marback <tmarback@sympho.dev>
2021-06-08 15:51:12 -0500

    Formatting.

--- src/main.cpp
+++ src/main.cpp
@@ -45,86 +45,86 @@ namespace redisfs {
 
     namespace fuse {
 
-    static struct redis_fs_info {
+        static struct redis_fs_info {
 
-        std::shared_ptr<RedisFS> fs;
+            std::shared_ptr<RedisFS> fs;
 
-    } redis_fs_info;
+        } redis_fs_info;
 
-    //required fuse functions
+        //required fuse functions
 
-    static void * init( struct fuse_conn_info *conn, struct fuse_config *cfg ) {
+        static void * init( struct fuse_conn_info *conn, struct fuse_config *cfg ) {
 
-        //TODO
-        //(void) conn;
-        //cfg->kernel_cache = 0;
+            //TODO
+            //(void) conn;
+            //cfg->kernel_cache = 0;
 
-        std::string host = "127.0.0.1"; // Required.
-        int         port = 7000;        // Optional. The default port is 6379.
-        const std::string & uri = host + ":" + std::to_string( port );
+            std::string host = "127.0.0.1"; // Required.
+            int         port = 7000;        // Optional. The default port is 6379.
+            const std::string & uri = host + ":" + std::to_string( port );
 
-        const redis::ConnectionOptions connectionOptions( uri );
-        std::shared_ptr<KVStore> store = std::make_shared<redis::RedisClusterStore>( connectionOptions );
+            const redis::ConnectionOptions connectionOptions( uri );
+            std::shared_ptr<KVStore> store = std::make_shared<redis::RedisClusterStore>( connectionOptions );
 
-        redis_fs_info.fs = std::make_shared<RedisFS>( store, BLOCK_SIZE );
+            redis_fs_info.fs = std::make_shared<RedisFS>( store, BLOCK_SIZE );
 
-        return ( void * ) &redis_fs_info;
-        
-    }
+            return ( void * ) &redis_fs_info;
+            
+        }
 
-    static int getattr( const char * path, struct stat * stbuf,
+        static int getattr( const char * path, struct stat * stbuf,
+                            struct fuse_file_info * fi ) {
+
+            return redis_fs_info.fs->getattr( path, stbuf );
+
+        }
+
+        static int readdir( const char * path, void * buf, fuse_fill_dir_t filler, off_t offset, 
+                            struct fuse_file_info * fi, enum fuse_readdir_flags flags ) {
+
+            ( void ) offset;
+            ( void ) fi;
+
+            return redis_fs_info.fs->readdir( path, buf, offset );
+
+        }
+
+        static int open( const char * path, struct fuse_file_info * fi ) {
+
+            return redis_fs_info.fs->open( path );
+            
+        }
+
+        static int read( const char * path, char * buf, size_t size, off_t offset,
                         struct fuse_file_info * fi ) {
 
-        return redis_fs_info.fs->getattr( path, stbuf );
+            ( void ) fi;
+            return redis_fs_info.fs->read( path, buf, size, offset );
+            
+        }
 
-    }
+        static int write( const char * path, const char * buf, size_t size, off_t offset, 
+                            struct fuse_file_info * fi ) {
 
-    static int readdir( const char * path, void * buf, fuse_fill_dir_t filler, off_t offset, 
-                        struct fuse_file_info * fi, enum fuse_readdir_flags flags ) {
+            return redis_fs_info.fs->write( path, buf, size, offset );
 
-        ( void ) offset;
-        ( void ) fi;
+        }
 
-        return redis_fs_info.fs->readdir( path, buf, offset );
+        int release( const char * path, struct fuse_file_info * fi ) {
 
-    }
+            return redis_fs_info.fs->release( path );
 
-    static int open( const char * path, struct fuse_file_info * fi ) {
+        }
 
-        return redis_fs_info.fs->open( path );
-        
-    }
-
-    static int read( const char * path, char * buf, size_t size, off_t offset,
-                    struct fuse_file_info * fi ) {
-
-        ( void ) fi;
-        return redis_fs_info.fs->read( path, buf, size, offset );
-        
-    }
-
-    static int write( const char * path, const char * buf, size_t size, off_t offset, 
-                        struct fuse_file_info * fi ) {
-
-        return redis_fs_info.fs->write( path, buf, size, offset );
-
-    }
-
-    int release( const char * path, struct fuse_file_info * fi ) {
-
-        return redis_fs_info.fs->release( path );
-
-    }
-
-    static const struct fuse_operations test_oper {
-        .getattr = getattr,
-        .open = open,
-        .read = read,
-        .write = write,
-        .release = release,
-        .readdir = readdir,
-        .init = init,
-    };
+        static const struct fuse_operations test_oper {
+            .getattr = getattr,
+            .open = open,
+            .read = read,
+            .write = write,
+            .release = release,
+            .readdir = readdir,
+            .init = init,
+        };
 
     }
 
