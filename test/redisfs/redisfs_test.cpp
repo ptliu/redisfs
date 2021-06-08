f73f79a8e2d3f65fdf8194a2f1f3f3c096bc02f4
Thiago Marback <tmarback@sympho.dev>
2021-06-08 11:15:17 -0500

    Added more read/write fs tests.

--- test/redisfs/redisfs_test.cpp
+++ test/redisfs/redisfs_test.cpp
@@ -1,5 +1,7 @@
 #include "redisfs/redisfs.h"
 
+#include <limits>
+#include <random>
 #include <unordered_map>
 
 #include <gtest/gtest.h>
@@ -35,13 +37,30 @@ class MemoryStore : public redisfs::KVStore {
 
 };
 
+static char * randomData( const size_t size ) {
+
+    constexpr size_t SEED = 4201337;
+    static std::default_random_engine generator( SEED );
+    static std::uniform_int_distribution<char> distribution( 
+        std::numeric_limits<char>::min(), 
+        std::numeric_limits<char>::max() 
+    );
+
+    char * data = ( char * ) malloc( size );
+    for ( size_t i = 0; i < size; i++ ) {
+        data[i] = distribution( generator );
+    }
+
+    return data;
+    
+}
+
 class RedisFSTest : public ::testing::Test {
 
     private:
     static std::pair<char *, size_t> makeItem( size_t size ) {
 
-        void * data = malloc( size ); // Random enough?
-        return std::make_pair( ( char * ) data, size );
+        return std::make_pair( randomData( size ), size );
 
     }
 
@@ -90,6 +109,100 @@ TEST_F( RedisFSTest, TestRead ) {
         char * buf = ( char * ) malloc( it.second.second );
         fs.read( it.first.c_str(), buf, it.second.second, 0 );
         EXPECT_EQ( memcmp( buf, it.second.first, it.second.second ), 0 );
+
+        free( buf );
+
+    }
+
+}
+
+TEST_F( RedisFSTest, TestOverwrite ) {
+
+    for ( auto it : baseItems ) {
+
+        const size_t modSize = it.second.second / 4;
+        const off_t modIdx = it.second.second / 2;
+        char * mod = randomData( modSize );
+
+        fs.write( it.first.c_str(), mod, modSize, modIdx );
+
+        char * res = ( char * ) malloc( it.second.second );
+        memcpy( res, it.second.first, it.second.second );
+
+        char * buf = ( char * ) malloc( it.second.second );
+        fs.read( it.first.c_str(), buf, it.second.second, 0 );
+        EXPECT_EQ( memcmp( buf, res, it.second.second ), 0 );
+
+        free( mod );
+        free( res );
+        free( buf );
+
+    }
+
+}
+
+TEST_F( RedisFSTest, TestAppend ) {
+
+    for ( auto it : baseItems ) {
+
+        char * mod = randomData( it.second.second );
+
+        fs.write( it.first.c_str(), mod, it.second.second, it.second.second );
+
+        const size_t newSize = it.second.second * 2;
+        char * res = ( char * ) malloc( newSize );
+        memcpy( res, it.second.first, it.second.second );
+        memcpy( res + it.second.second, mod, it.second.second );
+
+        char * buf = ( char * ) malloc( newSize );
+        fs.read( it.first.c_str(), buf, newSize, 0 );
+        EXPECT_EQ( memcmp( buf, res, newSize ), 0 );
+
+        free( mod );
+        free( res );
+        free( buf );
+
+    }
+
+}
+
+TEST_F( RedisFSTest, TestOverwriteAndAppend ) {
+
+    for ( auto it : baseItems ) {
+
+        const off_t modIdx = it.second.second / 2;
+        char * mod = randomData( it.second.second );
+
+        fs.write( it.first.c_str(), mod, it.second.second, modIdx );
+
+        const size_t newSize = modIdx + it.second.second;
+        char * res = ( char * ) malloc( newSize );
+        memcpy( res, it.second.first, modIdx );
+        memcpy( res + modIdx, mod, it.second.second );
+
+        char * buf = ( char * ) malloc( newSize );
+        fs.read( it.first.c_str(), buf, newSize, 0 );
+        EXPECT_EQ( memcmp( buf, res, newSize ), 0 );
+
+        free( mod );
+        free( res );
+        free( buf );
+
+    }
+
+}
+
+TEST_F( RedisFSTest, TestReadOffset ) {
+
+    for ( auto it : baseItems ) {
+
+        const off_t idx = it.second.second / 2;
+        const size_t size = it.second.second / 4;
+
+        char * buf = ( char * ) malloc( size );
+        fs.read( it.first.c_str(), buf, size, idx );
+        EXPECT_EQ( memcmp( buf, it.second.first + idx, size ), 0 );
+
         free( buf );
 
     }
