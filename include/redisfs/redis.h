28f345822788a7bce084767451f46345a928a169
Thiago Marback <tmarback@sympho.dev>
2021-06-09 09:44:09 -0500

    Added keystore for single redis server.

--- include/redisfs/redis.h
+++ include/redisfs/redis.h
@@ -10,6 +10,23 @@ namespace redisfs {
     namespace redis {
 
         /**
+         * @brief Store that redirects everything to a Redis server.
+         */
+        class RedisStore : public KVStore {
+
+            private:
+            sw::redis::Redis redis;
+
+            public:
+            RedisStore( const sw::redis::ConnectionOptions & options );
+
+            std::optional<std::string> get( const std::string_view & key ) override;
+            bool set( const std::string_view & key, const std::string_view & value ) override;
+            bool del( const std::string_view & key ) override;
+
+        };
+
+        /**
          * @brief Store that redirects everything to a Redis cluster.
          */
         class RedisClusterStore : public KVStore {
--- src/redisfs/redis.cpp
+++ src/redisfs/redis.cpp
@@ -1,5 +1,21 @@
 #include "redisfs/redis.h"
 
+/* RedisStore */
+
+redisfs::redis::RedisStore::RedisStore( const sw::redis::ConnectionOptions & options ) : redis( options ) {}
+
+std::optional<std::string> redisfs::redis::RedisStore::get( const std::string_view & key ) {
+    return redis.get( key );
+}
+bool redisfs::redis::RedisStore::set( const std::string_view & key, const std::string_view & value ) {
+    return redis.set( key, value );
+}
+bool redisfs::redis::RedisStore::del( const std::string_view & key ) {
+    return redis.del( key ) == 1;
+}
+
+/* RedisClusterStore */
+
 redisfs::redis::RedisClusterStore::RedisClusterStore( const sw::redis::ConnectionOptions & options ) : cluster( options ) {}
 
 std::optional<std::string> redisfs::redis::RedisClusterStore::get( const std::string_view & key ) {
