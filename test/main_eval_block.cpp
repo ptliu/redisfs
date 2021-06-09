cbf31e380185ea3c0c1873960198383ba6caa3e6
Thiago Marback <tmarback@sympho.dev>
2021-06-09 10:09:48 -0500

    Set up block eval structure.

--- test/main_eval_block.cpp
+++ test/main_eval_block.cpp
@@ -1,8 +1,36 @@
 #include <cstdlib>
 
-int main( void ) {
+#include "redisfs/kvstore.h"
+#include "redisfs/redis.h"
+
+void testRead( redisfs::KVStore & store ) {
 
     // TODO
+
+}
+
+void testWrite( redisfs::KVStore & store ) {
+
+    // TODO
+
+}
+
+int main( void ) {
+
+    std::string host = "127.0.0.1"; // Required.
+    int         port = 7000;        // Optional. The default port is 6379.
+    const std::string & uri = host + ":" + std::to_string( port );
+
+    const sw::redis::ConnectionOptions connectionOptions( uri );
+    redisfs::redis::RedisStore store( connectionOptions );
+
+    testRead( store );
+
+    store.clear();
+
+    testWrite( store );
+
+    store.clear();
 
     return EXIT_SUCCESS;
 
